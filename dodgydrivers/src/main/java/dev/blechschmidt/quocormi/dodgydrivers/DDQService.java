56de2054258f522bf6870fbbaea0f7f11b68829f
Til Blechschmidt <til@blechschmidt.de>
2022-09-26 13:55:33 +0100

    Reformat quotation services

--- dodgydrivers/src/main/java/dev/blechschmidt/quocormi/dodgydrivers/DDQService.java
+++ dodgydrivers/src/main/java/dev/blechschmidt/quocormi/dodgydrivers/DDQService.java
@@ -3,19 +3,18 @@ package dev.blechschmidt.quocormi.dodgydrivers;
 import dev.blechschmidt.quocormi.core.AbstractQuotationService;
 import dev.blechschmidt.quocormi.core.ClientInfo;
 import dev.blechschmidt.quocormi.core.Quotation;
-import dev.blechschmidt.quocormi.core.QuotationService;
 
 /**
  * Implementation of Quotation Service for Dodgy Drivers Insurance Company
- *  
+ * 
  * @author Rem
  *
  */
-public class DDQService extends AbstractQuotationService implements QuotationService {
+public class DDQService extends AbstractQuotationService {
 	// All references are to be prefixed with an DD (e.g. DD001000)
 	public static final String PREFIX = "DD";
 	public static final String COMPANY = "Dodgy Drivers Corp.";
-	
+
 	/**
 	 * Quote generation:
 	 * 5% discount per penalty point (3 points required for qualification)
@@ -26,19 +25,19 @@ public class DDQService extends AbstractQuotationService implements QuotationSer
 	public Quotation generateQuotation(ClientInfo info) {
 		// Create an initial quotation between 800 and 1000
 		double price = generatePrice(800, 200);
-		
+
 		// 5% discount per penalty point (3 points required for qualification)
-		int discount = (info.points > 3) ? 5*info.points:-50;
-		
+		int discount = (info.points > 3) ? 5 * info.points : -50;
+
 		// Add a no claims discount
 		discount += getNoClaimsDiscount(info);
-		
+
 		// Generate the quotation and send it back
-		return new Quotation(COMPANY, generateReference(PREFIX), (price * (100-discount)) / 100);
+		return new Quotation(COMPANY, generateReference(PREFIX), (price * (100 - discount)) / 100);
 	}
 
 	private int getNoClaimsDiscount(ClientInfo info) {
-		return 10*info.noClaims;
+		return 10 * info.noClaims;
 	}
 
 }
--- girlpower/src/main/java/dev/blechschmidt/quocormi/girlpower/GPQService.java
+++ girlpower/src/main/java/dev/blechschmidt/quocormi/girlpower/GPQService.java
@@ -3,7 +3,6 @@ package dev.blechschmidt.quocormi.girlpower;
 import dev.blechschmidt.quocormi.core.AbstractQuotationService;
 import dev.blechschmidt.quocormi.core.ClientInfo;
 import dev.blechschmidt.quocormi.core.Quotation;
-import dev.blechschmidt.quocormi.core.QuotationService;
 
 /**
  * Implementation of the Girl Power insurance quotation service.
@@ -11,11 +10,11 @@ import dev.blechschmidt.quocormi.core.QuotationService;
  * @author Rem
  *
  */
-public class GPQService extends AbstractQuotationService implements QuotationService {
+public class GPQService extends AbstractQuotationService {
 	// All references are to be prefixed with an DD (e.g. DD001000)
 	public static final String PREFIX = "GP";
 	public static final String COMPANY = "Girl Power Inc.";
-	
+
 	/**
 	 * Quote generation:
 	 * 50% discount for being female
@@ -29,30 +28,33 @@ public class GPQService extends AbstractQuotationService implements QuotationSer
 	public Quotation generateQuotation(ClientInfo info) {
 		// Create an initial quotation between 600 and 1000
 		double price = generatePrice(600, 400);
-		
+
 		// Automatic 50% discount for being female
-		int discount = (info.gender == ClientInfo.FEMALE) ? 50:0;
-		
+		int discount = (info.gender == ClientInfo.FEMALE) ? 50 : 0;
+
 		// Add a points discount
 		discount += getPointsDiscount(info);
-		
+
 		// Add a no claims discount
 		discount += getNoClaimsDiscount(info);
-		
+
 		// Generate the quotation and send it back
-		return new Quotation(COMPANY, generateReference(PREFIX), (price * (100-discount)) / 100);
+		return new Quotation(COMPANY, generateReference(PREFIX), (price * (100 - discount)) / 100);
 	}
 
 	private int getNoClaimsDiscount(ClientInfo info) {
-		return 5*info.noClaims;
+		return 5 * info.noClaims;
 	}
 
 	private int getPointsDiscount(ClientInfo info) {
-		if (info.points == 0) return 20;
-		if (info.points < 3) return 15;
-		if (info.points < 6) return 0;
+		if (info.points == 0)
+			return 20;
+		if (info.points < 3)
+			return 15;
+		if (info.points < 6)
+			return 0;
 		return -100;
-		
+
 	}
 
 }
