efd8977cc5fcbc21ae799b9b2ba72305595aa17f
Til Blechschmidt <til@blechschmidt.de>
2022-09-26 13:57:49 +0100

    Rename LocalBrokerService and add additional test case

---
+++ broker/src/main/java/dev/blechschmidt/quocormi/broker/RemoteBrokerService.java
@@ -0,0 +1,47 @@
+package dev.blechschmidt.quocormi.broker;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+
+import dev.blechschmidt.quocormi.core.BrokerService;
+import dev.blechschmidt.quocormi.core.ClientInfo;
+import dev.blechschmidt.quocormi.core.Quotation;
+import dev.blechschmidt.quocormi.core.QuotationService;
+
+/**
+ * Implementation of the broker service that uses the Service Registry.
+ * 
+ * @author Rem, Til Blechschmidt <til.blechschmidt@ucdconnect.ie>
+ *
+ */
+public class RemoteBrokerService implements BrokerService {
+    Registry registry;
+
+    public RemoteBrokerService(Registry registry) {
+        this.registry = registry;
+    }
+
+    public List<Quotation> getQuotations(ClientInfo info) throws RemoteException {
+        List<Quotation> quotations = new LinkedList<Quotation>();
+
+        for (String name : this.registry.list()) {
+            if (name.startsWith("qs-")) {
+                try {
+                    QuotationService service = (QuotationService) this.registry.lookup(name);
+                    quotations.add(service.generateQuotation(info));
+                } catch (Exception e) {
+                    // We do not want the client to ever encounter an error, so instead we print the
+                    // error and softly fall back to just returning the quotations that we can get.
+                    //
+                    // It should be noted that unresponsive services might block for a while before
+                    // returning errors, thus increasing response time.
+                    new Exception("Failed to query quotation service '" + name + "'", e).printStackTrace();
+                }
+            }
+        }
+
+        return quotations;
+    }
+}
--- broker/src/test/java/BrokerUnitTest.java
+++ broker/src/test/java/BrokerUnitTest.java
@@ -1,7 +1,6 @@
 import java.rmi.registry.Registry;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
@@ -12,38 +11,46 @@ import dev.blechschmidt.quocormi.core.Quotation;
 import dev.blechschmidt.quocormi.core.QuotationService;
 import dev.blechschmidt.quocormi.core.BrokerService;
 import dev.blechschmidt.quocormi.core.ClientInfo;
-import dev.blechschmidt.quocormi.broker.LocalBrokerService;
+import dev.blechschmidt.quocormi.broker.RemoteBrokerService;
 
 import dev.blechschmidt.quocormi.auldfellas.AFQService;
 
 import org.junit.*;
 
+// We are not using a static registry field w/ BeforeClass here because the two test cases require different setups
 public class BrokerUnitTest {
-    private static Registry registry;
+    private final static ClientInfo info = new ClientInfo("John", ClientInfo.MALE, 42, 1337, 69, "RZ-TB-363");
 
-    @BeforeClass
-    public static void setup() {
+    @Test
+    public void generateQuotationsTest() throws Exception {
         try {
-            registry = LocateRegistry.createRegistry(1099);
+            Registry registry = LocateRegistry.createRegistry(1099);
 
             QuotationService afqService = new AFQService();
-            BrokerService brokerService = new LocalBrokerService(registry);
+            BrokerService brokerService = new RemoteBrokerService(registry);
 
-            QuotationService quotationService = (QuotationService) UnicastRemoteObject.exportObject(afqService,0);
-            registry.bind(Constants.AULD_FELLAS_SERVICE, quotationService); 
+            QuotationService quotationService = (QuotationService) UnicastRemoteObject.exportObject(afqService, 0);
+            registry.bind(Constants.AULD_FELLAS_SERVICE, quotationService);
 
             BrokerService broker = (BrokerService) UnicastRemoteObject.exportObject(brokerService, 0);
             registry.bind(Constants.BROKER_SERVICE, broker);
+
+            BrokerService service = (BrokerService) registry.lookup(Constants.BROKER_SERVICE);
+            List<Quotation> quotations = service.getQuotations(info);
+            assertEquals(quotations.size(), 1);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Test
-    public void generateQuotationsTest() throws Exception {
-        BrokerService service = (BrokerService) registry.lookup(Constants.BROKER_SERVICE);
-        ClientInfo info = new ClientInfo("John", ClientInfo.MALE, 42, 1337, 69, "RZ-TB-363");
-        List<Quotation> quotations = service.getQuotations(info);
-        assertEquals(quotations.size(), 1);
+    public void generateQuotationsWithoutServicesTest() throws Exception {
+        // A different port has to be used because the tests may otherwise collide
+        // ("old" registry is still listening / the port is not yet freed)
+        Registry registry = LocateRegistry.createRegistry(1098);
+        BrokerService brokerService = new RemoteBrokerService(registry);
+
+        List<Quotation> quotations = brokerService.getQuotations(info);
+        assertEquals(quotations.size(), 0);
     }
 }
