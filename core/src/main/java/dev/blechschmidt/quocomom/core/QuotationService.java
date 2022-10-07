60ec77630416be559f70cdc90e31f823f6519e2a
Til Blechschmidt <til@blechschmidt.de>
2022-10-07 09:44:48 +0100

    T1 Project setup

---
+++ core/src/main/java/dev/blechschmidt/quocomom/core/QuotationService.java
@@ -0,0 +1,11 @@
+package dev.blechschmidt.quocomom.core;
+
+/**
+ * Interface to define the behaviour of a quotation service.
+ * 
+ * @author Rem
+ *
+ */
+public interface QuotationService {
+	public Quotation generateQuotation(ClientInfo info);
+}
